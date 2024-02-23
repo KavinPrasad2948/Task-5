728d5ef4d0b32d7e2eccba29b0d196378b5b2541
KavinPrasad2948 <kavinprasad2948@gmail.com>
2024-02-23 17:22:14 +0530

    completed

---
+++ Jsonformat/Resume.js
@@ -0,0 +1,34 @@
+const myResume = {
+    "name": "Kavin Prasad K",
+    "address": "5/535, Solai Nagar, Melur Road, Sivaganga, Tamil Nadu.",
+    "phone": "+91 9159663743",
+    "email": "kavinprasad2948@gmail.com",
+    "skills": {
+      "programmingLanguages": ["C", "C++", "Java", "Python", "JavaScript", "HTML", "CSS"],
+      "webDevelopment": ["HTML", "CSS", "PHP"],
+      "SoftSkills": ["Relationship Building", "Decision-making", "Critical Thinking", "Friendly", "Positive Attitude", "Reliability", "Trustworthiness"]
+    },
+    "education": {
+      "bachelor": "Bachelor in Mechatronics Engineering, Dr.Mahalingam College of Engineering And Technology, 2018 - 2022",
+      "highSchool": "High School, 21st Century International Matriculation Higher Secondary School"
+    },
+    "project": {
+      "objective": "Protect household and electronic appliances from voltage fluctuations.",
+      "functionality": [
+        "Measures input voltage from the main electrical line.",
+        "Converts voltage to a digital value using an ADC converter.",
+        "Activates an alarm buzzer if voltage exceeds 230V or falls below 220V.",
+        "Trips the MCB (Miniature Circuit Breaker) for added safety.",
+        "Displays current voltage level on an LCD."
+      ],
+      "programmingLanguageUsed": "Embedded C",
+      "developmentKit": "Arduino"
+    },
+    "workshop": {
+      "name": "MYCAPTAIN Artificial Intelligence workshop",
+      "date": "October 2020"
+    }
+  };
+
+  console.log(myResume);
+  
---
+++ README.md
@@ -0,0 +1,3 @@
+# TASK 5
+
+> In This repository i Created my Resume in JSON Format and performed in'For loop','ForEach()','for...In','for...Of'. 
